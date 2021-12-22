c4b39ac7fb8f0416038a4b501de7e40682e708bb
henriquelopes42 <lsiqueira@itelinfo.com>
2021-12-22 09:23:09 -0400

    Primeiro commit

---
+++ src/main/java/com/example/ProjetoLeilao/ProjetoLeilaoApplication.java
@@ -0,0 +1,17 @@
+package com.example.ProjetoLeilao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLeilaoApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Iniciando o programa");
+		SpringApplication.run(ProjetoLeilaoApplication.class, args);
+		System.out.println("Programa inicializado.");
+
+	}
+
+}
