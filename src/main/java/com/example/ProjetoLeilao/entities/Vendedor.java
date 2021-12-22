f98aeecfed11f7042e18096114594c2741e80900
henriquelopes42 <lsiqueira@itelinfo.com>
2021-12-22 11:14:28 -0400

    Adicionado entidade Vendedor

--- src/main/java/com/example/ProjetoLeilao/entities/Vendedor.java
+++ src/main/java/com/example/ProjetoLeilao/entities/Vendedor.java
@@ -1,4 +1,78 @@
 package com.example.ProjetoLeilao.entities;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "vendedor")
 public class Vendedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer idVendedor;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "data_nascimento")
+    private Date dataNascimento;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "ativo")
+    private Boolean ativo;
+
+
+    public Integer getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(Integer idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
 }
