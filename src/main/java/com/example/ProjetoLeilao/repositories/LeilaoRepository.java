7bdbbfe8d77853d5c9e2893e6b392938128958ea
morgenstern28 <mcrivelente@itelinfo.com>
2021-12-23 08:07:22 -0400

    Criando repository leilao

--- src/main/java/com/example/ProjetoLeilao/repositories/LeilaoRepository.java
+++ src/main/java/com/example/ProjetoLeilao/repositories/LeilaoRepository.java
@@ -1,4 +1,12 @@
 package com.example.ProjetoLeilao.repositories;
 
-public interface LeilaoRepository {
+import com.example.ProjetoLeilao.entities.Leilao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LeilaoRepository  extends JpaRepository<Leilao, Integer> {
+    List<Leilao> findByAtivo(Boolean ativo);
 }
