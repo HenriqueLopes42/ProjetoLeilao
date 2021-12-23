c0a13e744d0ee64c5d85776103e9520d3717c362
morgenstern28 <mcrivelente@itelinfo.com>
2021-12-23 08:10:44 -0400

    Criando raca repository

---
+++ src/main/java/com/example/ProjetoLeilao/repositories/RacaRepository.java
@@ -0,0 +1,12 @@
+package com.example.ProjetoLeilao.repositories;
+
+import com.example.ProjetoLeilao.entities.Raca;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RacaRepository extends JpaRepository<Raca, Integer> {
+    List<Raca> findByAtivo(Boolean ativo);
+}
