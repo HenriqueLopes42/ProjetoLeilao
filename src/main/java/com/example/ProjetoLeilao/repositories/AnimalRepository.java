27600c9e469a7761ea88b49525bb341725e778ef
henriquelopes42 <lsiqueira@itelinfo.com>
2021-12-23 08:32:36 -0400

    Adicionado repository Animal

---
+++ src/main/java/com/example/ProjetoLeilao/repositories/AnimalRepository.java
@@ -0,0 +1,12 @@
+package com.example.ProjetoLeilao.repositories;
+
+import com.example.ProjetoLeilao.entities.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Integer> {
+    List<Animal> findbyAtivo(Boolean ativo);
+}
