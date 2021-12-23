e74f47d17512ed8d11b9dd285bee0a24befdcd18
puleu <puleoti@outlook.com>
2021-12-23 09:10:39 -0400

    Adicionado Repositorio Medico

---
+++ src/main/java/com/example/ProjetoLeilao/repositories/MedicoRepository.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoLeilao.repositories;
+
+import com.example.ProjetoLeilao.entities.Medico;
+import com.example.ProjetoLeilao.entities.Raca;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Integer> {
+    List<Medico> findByAtivo(Boolean ativo);
+}
