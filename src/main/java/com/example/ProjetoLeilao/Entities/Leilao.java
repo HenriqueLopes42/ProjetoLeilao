300a741faf1dfb3d60e027421ab72dd4f684d37f
morgenstern28 <mcrivelente@itelinfo.com>
2021-12-23 07:34:38 -0400

    Criando entidade leilao

---
+++ src/main/java/com/example/ProjetoLeilao/Entities/Leilao.java
@@ -0,0 +1,39 @@
+package com.example.ProjetoLeilao.Entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="leilao")
+public class Leilao {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private Integer idLeilao;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "data")
+    private Date data;
+
+    @Column(name = "ativo")
+    private Boolean ativo;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public Date getData() { return data; }
+    public void setData(Date data) { this.data = data; }
+
+    public Boolean getAtivo() { return ativo; }
+    public void setAtivo(Boolean ativo) { this.ativo = ativo; }
+
+}
