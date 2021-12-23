ff31366ea7b416ca706d1d70a695c6503fde391a
puleu <puleoti@outlook.com>
2021-12-23 09:22:27 -0400

    Adicionado CompradorController Listar - Buscar

--- src/main/java/com/example/ProjetoLeilao/controllers/CompradorController.java
+++ src/main/java/com/example/ProjetoLeilao/controllers/CompradorController.java
@@ -1,6 +1,44 @@
 package com.example.ProjetoLeilao.controllers;
 
+import com.example.ProjetoLeilao.entities.Comprador;
+import com.example.ProjetoLeilao.entities.Medico;
+import com.example.ProjetoLeilao.repositories.CompradorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("Comprador")
+
 public class CompradorController {
+
+    @Autowired
+    public CompradorRepository compradorRepository;
+
+    //Incusão da lista
+    @GetMapping()
+    public List<Comprador> listar () {
+
+        List<Comprador> lista = compradorRepository.findByAtivo(true);
+        return lista;
+    }
+
+    @GetMapping()
+    public Comprador buscar (@PathVariable int id ){
+
+        Comprador busca = compradorRepository.findById(id).get();
+        return busca;
+
+    }
+
+  /*  @PostMapping
+    public Comprador incluir (@RequestBody Comprador comprador){
+
+        C
+    }*/
+
+
 
 
 }
