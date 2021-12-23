ba5e64f1777dc5b32af88dc7b7d2f5da82547aa7
morgenstern28 <jmedeiros@itelinfo.com>
2021-12-23 08:19:12 -0400

    Adicionando repository de comprador

---
+++ src/main/java/com/example/ProjetoLeilao/repositories/CompradorRepository.java
@@ -0,0 +1,14 @@
+package com.example.ProjetoLeilao.repositories;
+
+
+import com.example.ProjetoLeilao.entities.Comprador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CompradorRepository extends JpaRepository<Comprador, Integer> {
+    List<Comprador> findByAtivo (Boolean ativo);
+
+}
