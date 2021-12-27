24ebcc35b995112476e87f73430715687a06dac9
puleu <puleoti@outlook.com>
2021-12-27 08:52:46 -0400

    Controller Comprador Att

--- src/main/java/com/example/ProjetoLeilao/controllers/CompradorController.java
+++ src/main/java/com/example/ProjetoLeilao/controllers/CompradorController.java
@@ -1,5 +1,6 @@
 package com.example.ProjetoLeilao.controllers;
 
+import com.example.ProjetoLeilao.Mensagem;
 import com.example.ProjetoLeilao.entities.Comprador;
 import com.example.ProjetoLeilao.entities.Medico;
 import com.example.ProjetoLeilao.repositories.CompradorRepository;
@@ -31,6 +32,47 @@ public class CompradorController {
         return busca;
 
     }
+
+    @PostMapping
+    public Mensagem incluir(@RequestBody Comprador comprador) {
+        Mensagem msg = new Mensagem();
+        comprador.setIdComprador(0);
+        comprador.setAtivo(true);
+        compradorRepository.save(comprador);
+        compradorRepository.flush();
+        msg.setMensagem("Registro inserido.");
+        return msg;
+    }
+
+    @PutMapping
+    public Mensagem alterar(@RequestBody Comprador comprador) {
+        compradorRepository.save(comprador);
+        compradorRepository.flush();
+        Mensagem msg = new Mensagem();
+        msg.setMensagem("Registro alterado.");
+        return msg;
+    }
+
+    @DeleteMapping
+    public Mensagem deletar(@RequestBody Comprador comprador) {
+        Comprador excluir = compradorRepository.findById(comprador.getIdComprador()).get();
+        excluir.setAtivo(false);
+        Mensagem msg = new Mensagem();
+        msg.setMensagem("Registro excluido.");
+        return msg;
+    }
+
+    @DeleteMapping("{id}")
+    public Mensagem deletar(@PathVariable Integer id) {
+        Comprador excluir = compradorRepository.findById(id).get();
+        excluir.setAtivo(false);
+        compradorRepository.save(excluir);
+        compradorRepository.flush();
+        Mensagem msg = new Mensagem();
+        msg.setMensagem("Registro excluido.");
+        return msg;
+    }
+
 
 
 }
