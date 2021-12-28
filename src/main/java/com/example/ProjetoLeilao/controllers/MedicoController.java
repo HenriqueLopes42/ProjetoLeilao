3dc66a0097377d9e0637b290e53e0b7633d045c3
morgenstern28 <jmedeiros@itelinfo.com>
2021-12-28 10:46:12 -0400

    att

--- src/main/java/com/example/ProjetoLeilao/controllers/MedicoController.java
+++ src/main/java/com/example/ProjetoLeilao/controllers/MedicoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("medico")
+@CrossOrigin
 public class MedicoController {
 
    @Autowired
--- src/main/java/com/example/ProjetoLeilao/controllers/RacaController.java
+++ src/main/java/com/example/ProjetoLeilao/controllers/RacaController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("raca")
+@CrossOrigin
 public class RacaController {
     @Autowired
     private RacaRepository racaRepository;
