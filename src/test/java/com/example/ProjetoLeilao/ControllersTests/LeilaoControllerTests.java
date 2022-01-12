58f4574a95ee914add6d78b4a5956ede49b4ee6b
AndreAraujo1998 <andre.florencio98@gmail.com>
2022-01-11 23:56:34 -0400

    concertando erros diversos

--- src/test/java/com/example/ProjetoLeilao/ControllersTests/LeilaoControllerTests.java
+++ src/test/java/com/example/ProjetoLeilao/ControllersTests/LeilaoControllerTests.java
@@ -11,57 +11,78 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class LeilaoControllerTests {
 
     @Autowired
     private LeilaoController leilaoController;
+    
+    
+    Leilao leilao = new Leilao(); // Olhar construtor e quais parametros passar pra apagar as linhas de baixo
+    leilao.setIdLeilao(0);
+    leilao.setAtivo(true);
+    leilao.setNome("André");
+    leilao.setData(new Date());
+    leilao.setDescricao("Descricao");
 
-    @Test
-    public void listarTest() {
-        Boolean expected = false;
-        Boolean result = false;
+    @BeforeEach
+    public void purposelessMethod() {
 
-        try {
-            List<Leilao> lista = leilaoController.listar();
-            System.out.println("Teste de listar leilao");
-            result = true;
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-            result = false;
-        }
     }
 
     @Test
-    public void incluirTest(){
+    public void deveRetornarSuccessQuandoListarLeiloes() {
+        Boolean expected = true;
+        Boolean result = false;
 
-        Boolean expected = false;
+        try {
+            List<Leilao> list = leilaoController.listar();
+            System.out.println("Teste de listar leilões");
+            result = true;
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            result = false;
+
+        }
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void deveRetornarSuccessQuandoAdicionarLeilao(){
+        Boolean expected = true;
         Boolean result = false;
 
         try {
             int quantidadeAnterior = this.leilaoController.listar().size();
 
-            Leilao leilao = new Leilao();
-            leilao.setIdLeilao(0);
-            leilao.setAtivo(true);
-            leilao.setNome("André");
-            leilao.setData(new Date());
-            leilao.setDescricao("Descricao");
-            int quantidadeNova = this.leilaoController.listar().size();
             Mensagem msg = this.leilaoController.incluir(leilao);
-            if (quantidadeNova == quantidadeAnterior + 1) {
+            int quantidadeNova = this.leilaoController.listar().size(); // não seria incluir?
+            
+            if (quantidadeNova == quantidadeAnterior + 1)
                 result = true;
-            } else {
-                System.out.println("Nao incluiu leilão: " + msg.getMensagem());
-                result = false;
+            else {
+                System.out.println("Nao foi possível incluir o leilão: " + msg.getMensagem());
+                // result = false;
                 for( String s: msg.getErros()){
                     System.out.println(s);
                 }
             }
-        } catch ( Exception ex){
-            System.out.println("Erro ao incluir animal: " + ex.getMessage());
-            result = false;
+        } catch ( Exception ex ){
+            System.out.println("Erro ao incluir o leilão: " + ex.getMessage());
+            // result = false;
         }
 
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void testeDeAlterar() {
+        
+        try {
+            
+        }
+            
+        Assertions.assertEquals()
     }
 }
