b509e2def73dfe54c35b4bff3b862b43b532119f
AndreAraujo1998 <aflorencio@itelinfo.com>
2022-01-12 09:45:14 -0400

    Adicionados métodos pra testar a busca, a alteração e a exclusão

--- src/test/java/com/example/ProjetoLeilao/ControllersTests/LeilaoControllerTests.java
+++ src/test/java/com/example/ProjetoLeilao/ControllersTests/LeilaoControllerTests.java
@@ -25,6 +25,28 @@ public class LeilaoControllerTests {
     }
 
     @Test
+    public void buscarTest() {
+
+        Boolean expected = true;
+        Boolean result = false;
+
+        try {
+            Leilao leilao = leilaoController.buscar(1);
+            if (leilao.getIdLeilao() != null) { // Trocar para ternário
+                result = true;
+            } else {
+                result = false;
+                System.out.println("Leilao - BuscarTest - Registro de Leilao nao encontrado");
+            }
+
+        } catch (Exception ex) {
+            result = false;
+            System.out.println("Erro ao buscar Leilao" + ex.getMessage());
+        }
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     public void listarTest() {
         Boolean expected = true;
         Boolean result = true;
@@ -47,18 +69,16 @@ public class LeilaoControllerTests {
         Boolean expected = true;
         Boolean result = false;
 
-
-
         try {
             Leilao leilao = new Leilao();
-            leilao = this.leilaoController.buscar(1);
-            leilao.setNome("Leilao de teste");
-            leilao.setAtivo(true);
-
             int quantidadeAnterior = this.leilaoController.listar().size();
 
+            leilao = this.leilaoController.buscar(1);
+            leilao.setNome("Leilao teste" + (quantidadeAnterior));
+            leilao.setAtivo(true);
+
             Mensagem msg = this.leilaoController.incluir(leilao);
-            int quantidadeNova = this.leilaoController.listar().size(); // não seria incluir?
+            int quantidadeNova = this.leilaoController.listar().size();
             
             if (quantidadeNova == quantidadeAnterior + 1)
                 result = true;
@@ -77,19 +97,96 @@ public class LeilaoControllerTests {
         assertThat(result).isEqualTo(expected);
     }
 
-
     @Test
     public void alterarTest() {
         Boolean expected = true;
         Boolean result = false;
 
         try {
-             // Mensagem msg = this.leilaoController.alterar(leilao);
-             result = true;
+             Leilao leilao = this.leilaoController.buscar(8);
+             leilao.setNome("Leilao teste" + (int)(Math.random() * 100));
+             this.leilaoController.alterar(leilao);
+             // Mensagem msg = this.leilaoController.alterar(leilao); -- Mensagem nao esta funcionando
+
+            Leilao leilaoAlterado = this.leilaoController.buscar(8);
+            if (leilao.getNome().equals(leilaoAlterado.getNome())) {
+                result = true;
+            } else {
+                result = false;
+                System.out.println("leilao - AlterarTest: o nome nao foi alterado.");
+                System.out.println(leilao.getNome());
+                System.out.println(leilaoAlterado.getNome());
+
+                /*
+                for (String erro : msg.getErros()) {
+                    System.out.println(erro);
+                }
+                */
+            }
         } catch ( Exception ex ) {
             System.out.println("Erro ao alterar o leilão: " + ex.getMessage());
             result = false;
         }
+        assertThat(expected).isEqualTo(result);
         // Assertions.assertEquals();
+    }
+
+    @Test
+    public void deletarTest() {
+
+        Boolean expected = true;
+        Boolean result = false;
+
+        try {
+
+            Leilao leilao = this.leilaoController.buscar(8);
+            leilao.setAtivo(true);
+            this.leilaoController.alterar(leilao); // Pode dar erro se ja estava ativo, mas vai seguir o baile
+
+            Integer idRegistroaExcluir = leilao.getIdLeilao();
+            this.leilaoController.deletar(idRegistroaExcluir);
+
+            leilao = this.leilaoController.buscar(8);
+
+            result = !leilao.getAtivo() ? true : false; // Copiar pro outro que pode ser ternário
+
+            leilao.setAtivo(true);
+            this.leilaoController.alterar(leilao);
+        } catch (Exception ex) {
+            result = false;
+            System.out.println("Leilao deletar: " + ex.getMessage());
+        }
+
+
+        assertThat(result).isEqualTo(expected);
+
+    }
+
+    @Test
+    public void deletarByObjectTest() {
+        Boolean expected = true;
+        Boolean result = false;
+
+        try {
+
+            Leilao leilao = this.leilaoController.buscar(8);
+            leilao.setAtivo(true);
+            this.leilaoController.alterar(leilao); // Pode dar erro se ja estava ativo, mas vai seguir o baile
+
+            this.leilaoController.deletar(leilao);
+
+            leilao = this.leilaoController.buscar(8);
+
+            result = !leilao.getAtivo() ? true : false; // Copiar pro outro que pode ser ternário
+
+            leilao.setAtivo(true);
+            this.leilaoController.alterar(leilao);
+        } catch (Exception ex) {
+            result = false;
+            System.out.println("Leilao deletar: " + ex.getMessage());
+        }
+
+
+        assertThat(result).isEqualTo(expected);
     }
 }
